b88aae34930e6954694b46f8b8fb83e34d6c8f07
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-11-01 10:48:43 -0600

    Set up initial file structure and connect to Redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './containers/App';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import { rootReducer } from './reducers'
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>, 
+  document.getElementById('root'));
+
+
+
