e6cece5cc44000ae5482738b77fb351ae342f2b0
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-11-02 12:37:51 -0600

    Add fetchCalls for all news sources

--- src/utilities/helper.js
+++ src/utilities/helper.js
@@ -1,8 +1,36 @@
 import { key } from './apiKey';
 
-export const getNews = async() => {
-  const response = await fetch(`https://newsapi.org/v2/everything?q=metoo&domains= breitbart.com,  theamericanconservative.com&from=2018-10-22&to=2018-10-28&sortBy=popularity&apiKey=${key}`);
+export const getNews = async(category) => {
+  const leftResponse = await getNewsFromLeft(category);
+  const centerResponse = await getNewsFromCenter(category);
+  const rightResponse = await getNewsFromRight(category);
+  const allNews = {
+    left: {...leftResponse},
+    center: {...centerResponse},
+    right: {...rightResponse}
+  }
+  return allNews
+};
+
+
+
+export const getNewsFromLeft = async(category) => {
+  const response = await fetch(`https://newsapi.org/v2/everything?q=${category}&domains=huffingtonpost.com,msnbc.com&sortBy=publishedAt&apiKey=${key}`);
+  const data = await response.json();
+  return data 
+};
+
+export const getNewsFromCenter = async(category) => {
+  const response = await fetch(`https://newsapi.org/v2/everything?q=${category}&domains=npr.org,bbc.com,cnn.com&sortBy=publishedAt&apiKey=${key}`);
+  const data = await response.json();
+  return data 
+};
+
+export const getNewsFromRight = async(category) => {
+  const response = await fetch(`https://newsapi.org/v2/everything?q=${category}&domains=breitbart.com,foxnews.com&sortBy=publishedAt&apiKey=${key}`);
   const data = await response.json();
   console.log(data);
+  return data
+};
 
-}
+
