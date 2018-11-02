0b34e6833220264cdb14693eee2352b8862fdd32
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-11-02 12:38:19 -0600

    Add click events on the home page buttons

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -5,15 +5,48 @@ import './App.css';
 class App extends Component {
 
   componentDidMount() {
-    getNews();
+  
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1> news-ocracy</h1>
+          <h1 className="title"> news-ocracy</h1>
+          <h5 className="sub-title"> seek perspectives.  identify bias.  engage in civil discourse. </h5>
         </header>
+        <section className="button-container">
+          <button 
+            className="topic-button"
+            onClick={()=>getNews('immigration')}
+          >
+            Immigration
+          </button>
+          <button 
+            className="topic-button"
+            onClick={()=>getNews('climate')}
+          >
+            Climate Change
+          </button>
+          <button 
+            className="topic-button"
+            onClick={()=>getNews('midterm, elections')}
+          >
+            Midterm Elections
+          </button>
+          <button 
+            className="topic-button"
+            onClick={()=>getNews('healthcare')}
+          >
+            Healthcare
+          </button>
+          <button 
+            className="topic-button"
+            onClick={()=>getNews('refugees')}
+          >
+            Refugees
+          </button>
+        </section>
       </div>
     );
   }
