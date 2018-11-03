92018cbece8148570c8a77ee6de6c8d53fe6caa3
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-11-03 16:44:43 -0600

    Refactor NewsTitle and NewsContainer so that news is rendering correctly on each route

--- src/components/NewsTitle/index.js
+++ src/components/NewsTitle/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
-export const NewsTitle = ({ leftArticle, centerArticle, rightArticle }) => {
-  console.log(leftArticle.title)
+export const NewsTitle = ({ article }) => {
 
 
   return (
     <div>
+    
+    {
+      article.category ==='left' && 
+        <article>
+          <h3>{article.title} <span>{article.author}</span></h3>
+        </article> 
+    }
+
+    {
+      article.category ==='center' && 
+        <article>
+          <h3>{article.title} <span>{article.author}</span></h3>
+        </article> 
+    }
+
+    {
+      article.category ==='right' && 
+        <article>
+          <h3>{article.title} <span>{article.author}</span></h3>
+        </article> 
+    }
+
     </div>
   )
+  
 }
--- src/containers/NewsContainer/index.js
+++ src/containers/NewsContainer/index.js
@@ -4,38 +4,22 @@ import { NewsTitle } from '../../components/NewsTitle';
 
 export const NewsContainer = (props) => {
   const {articles} = props;
-
-  console.log(typeof articles)
-  console.log(typeof articles.left)
+  console.log(articles)
+  
 
   let uuidv4 = require("uuid/v4");
   
-  // const displayLeft = articles.left.map(leftArticle => (
-  //   <NewsTitle
-  //     leftArticle={leftArticle}
-  //     key={uuidv4()}
-  //   />
-  // ))
+  const displayArticles = articles.map(article => (
+    <NewsTitle
+      article={article}
+      key={uuidv4()}
+    />
+  ))
 
-  // const displayCenter = articles.center.map(centerArticle => (
-  //   <NewsTitle
-  //     centerArticle={centerArticle}
-  //     key={uuidv4()}
-  //   />
-  // ))
-
-  // const displayRight = articles.right.map(rightArticle => (
-  //   <NewsTitle
-  //     rightArticle={rightArticle}
-  //     key={uuidv4()}
-  //   />
-  // ))
   
   return (
     <section>
-      <article className='left-container'>  </article>
-      <article className='center-container'>  </article>
-      <article className='right-container'>  </article>
+      { displayArticles }
     </section>
   )
 };
