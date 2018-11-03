5e57e58ed40ac4193f469183240faf93d43247db
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-11-03 15:54:06 -0600

    Set up mock test for app and setupTests file

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
+import App from './index.js';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('should match the snapshot', () => {
+ const wrapper = shallow(<App />);
+ expect(wrapper).toMatchSnapshot();
 });
