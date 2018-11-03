bf5437ebcb7e79bea95e37f2f492c9eab8ed478c
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-11-03 07:42:31 -0600

    Refactor NewsContainer with separate maps for left, center, and right

--- src/containers/NewsContainer/index.js
+++ src/containers/NewsContainer/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { NewsTitle } from '../../components/NewsTitle';
 
 export const NewsContainer = () => {
 
+  const {articles} = props;
+  let uuidv4 = require("uuid/v4");
+
+  const displayLeft = articles.left.map(leftArticle => (
+    <NewsTitle
+      {...leftArticle}
+      key={uuidv4()}
+    />
+  ))
+
+  const displayCenter = articles.center.map(centerArticle => (
+    <NewsTitle
+      {...centerArticle}
+      key={uuidv4()}
+    />
+  ))
+
+  const displayRight = articles.right.map(rightArticle => (
+    <NewsTitle
+      {...rightArticle}
+      key={uuidv4()}
+    />
+  ))
+  
   return (
-    <div>
-    </div>
+    <section>
+      <article className='left-container'> {displayLeft} </article>
+      <article className='center-container'> {displayCenter} </article>
+      <article className='right-container'> {displayRight} </article>
+    </section>
   )
 };
 
