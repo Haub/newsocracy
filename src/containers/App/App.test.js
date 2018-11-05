057048148b44b25cba45f0e2808249a69e0bd9f0
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-11-04 19:59:23 -0700

    Add tests for App component

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './index.js';
+import { mapStateToProps, mapDispatchToProps } from './index.js';
+import { addCategoryNews } from '../../actions';
 
-it('should match the snapshot', () => {
- const wrapper = shallow(<App />);
- expect(wrapper).toMatchSnapshot();
+describe('APP', () => { 
+  it('should match the snapshot', () => {
+   const wrapper = shallow(<App />);
+   expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call ', () => {
+
+  });
+})
+
+describe('mapStateToProps', () => {
+  it('should have access to the articles array', () => {
+    const mockStore = {
+      articles: []
+    };
+    const expected = {...mockStore};
+    const result = mapStateToProps(mockStore);
+    expect(result).toEqual(expected);
+  });
 });
+
+describe('mapDispatchToProps', () => {
+  it('should add articles to the store with addCategoryNews', () => {
+    const mockDispatch = jest.fn();
+    const actiontoDispatch = addCategoryNews([]);
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.addCategoryNews([]);
+    expect(mockDispatch).toHaveBeenCalledWith(actiontoDispatch);
+  });
+})
