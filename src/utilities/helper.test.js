87c0d4ef50fb4586cce712d5b868ca8adfbfb7cf
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-11-05 13:34:19 -0700

    Add tests for failed fetch calls

--- src/utilities/helper.test.js
+++ src/utilities/helper.test.js
@@ -13,22 +13,28 @@ describe('HELPER', () => {
     expect(window.fetch).toHaveBeenCalledWith(expected);
   });
 
-  it('should return left news articles', async () => {
+  it('should return left news articles if the fetch is ok', async () => {
     const mockArticle = {title: 'News Story', source: 'CNN.com'};
-    const expected = {title: 'News Story', source: 'CNN.com'};
     window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       ok: true,
-      json: () => Promise.resolve( data )
+      json: () => Promise.resolve( mockArticle )
     }));
-    await getLeftNews();
-    expect(window.fetch).toHaveBeenCalledWith(...expected);
+    const result = await getLeftNews();
+
+    expect(result).toEqual(mockArticle);
   });
 
-  it('should return false if no data comes back', () => {
+  it('should throw an error if the getLeftNews fails', async () => {
+    const url = `https://newsapi.org/v2/everything?q=undefined&domains=huffingtonpost.com,msnbc.com&sortBy=publishedAt&apiKey=${key}`;
+    const expected = new Error('failed');
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject(new Error('failed'));
+    });
+    await expect(getLeftNews(url)).rejects.toEqual(expected);
   });
 
   it('should call getCenterNews with the correct params', async() => {
-    const expected = `https://newsapi.org/v2/everything?q=undefined&domains=domains=npr.org,bbc.com,cnn.com&sortBy=publishedAt&apiKey=${key}`;
+    const expected = `https://newsapi.org/v2/everything?q=undefined&domains=npr.org,bbc.com,cnn.com&sortBy=publishedAt&apiKey=${key}`;
     window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
         ok: true,
         json: () => Promise.resolve(expected)
@@ -37,15 +43,23 @@ describe('HELPER', () => {
     expect(window.fetch).toHaveBeenCalledWith(expected);
   });
 
-  it('should return center news articles', async () => {
+  it('should return center news articles if the fetch is ok', async () => {
     const mockArticle = {title: 'News Story', source: 'CNN.com'};
-    const expected = {title: 'News Story', source: 'CNN.com'};
     window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       ok: true,
-      json: () => Promise.resolve( data )
+      json: () => Promise.resolve( mockArticle )
     }));
-    await getCenterNews();
-    expect(window.fetch).toHaveBeenCalledWith(...expected);
+    const result = await getCenterNews();
+    expect(result).toEqual(mockArticle);
+  });
+
+  it('should throw an error if the getCenterNews fails', async () => {
+    const url = `https://newsapi.org/v2/everything?q=undefined&domains=domains=npr.org,bbc.com,cnn.com&sortBy=publishedAt&apiKey=${key}`;
+    const expected = new Error('failed');
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject(new Error('failed'));
+    });
+    await expect(getCenterNews(url)).rejects.toEqual(expected);
   });
 
   it('should call getRightNews with the correct params', async() => {
@@ -63,10 +77,19 @@ describe('HELPER', () => {
     const expected = {title: 'News Story', source: 'CNN.com'};
     window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       ok: true,
-      json: () => Promise.resolve( data )
+      json: () => Promise.resolve( mockArticle )
     }));
-    await getRightNews();
-    expect(window.fetch).toHaveBeenCalledWith(expected);
+    const result = await getRightNews();
+    expect(result).toEqual(mockArticle);
+  });
+
+  it('should throw an error if the getRightNews fails', async () => {
+    const url = `https://newsapi.org/v2/everything?q=undefined&domains=breitbart.com,foxnews.com&sortBy=publishedAt&apiKey=${key}`;
+    const expected = new Error('failed');
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject(new Error('failed'));
+    });
+    await expect(getRightNews(url)).rejects.toEqual(expected);
   });
 
 })
