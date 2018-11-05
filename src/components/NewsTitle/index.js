3468c7a8ba130d261a0f199fff0a3a0862f88be3
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-11-04 17:29:31 -0700

    Refactor NewsTitle and NewsContainer to get articles displayed by category

--- src/components/NewsTitle/index.js
+++ src/components/NewsTitle/index.js
@@ -3,42 +3,15 @@ import './NewsTitle.css';
 
 export const NewsTitle = ({ article }) => {
 
-
   return (
     <div className='container'>
-    
-    {
-      article.category ==='left' && 
-        <article className='left column'>
-          <img src={article.image} height='30px' width='30px' alt='' />
-            <h3>{article.title} 
-            <p>{article.source.name}</p>
-            <p>{article.date}</p>
-            </h3>
-        </article> 
-    }
-
-    {
-      article.category ==='center' && 
-        <article className='center column'>
-          <img src={article.image} height='30px' width='30px' alt='' />
+      <article className='left column'>
           <h3>{article.title} 
-            <p>{article.source.name}</p>
+          <p>{article.source.name}</p>
+          <p>{article.date}</p>
           </h3>
-        </article> 
-    }
-
-    {
-      article.category ==='right' && 
-        <article className='right column'>
-        <img src={article.image} height='30px' width='30px' alt='' />
-          <h3>{article.title} 
-            <p>{article.source.name}</p>
-          </h3>
-        </article> 
-    }
-
+      </article> 
     </div>
   )
-  
-}
+
+};
