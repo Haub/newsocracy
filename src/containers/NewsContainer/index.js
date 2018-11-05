9051ab319d6728b6811a40865e97eccd07cf2501
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-11-04 19:59:47 -0700

    Add additional tests including snapshot tests

--- src/containers/NewsContainer/index.js
+++ src/containers/NewsContainer/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { NewsTitle } from '../../components/NewsTitle';
+import './NewsContainer.css';
 
 export const NewsContainer = (props) => {
   let uuidv4 = require("uuid/v4");
   const {articles} = props;
-  
-  const displayArticles = articles.map(article => (
+
+  const displayLeft = articles.slice(0, 20).map(article => (
+    <NewsTitle
+      article={article}
+      key={uuidv4()}
+    />
+  ));
+
+  const displayCenter = articles.slice(20,40).map(article => (
+    <NewsTitle
+      article={article}
+      key={uuidv4()}
+    />
+  ));
+
+  const displayRight = articles.slice(40, 60).map(article => (
     <NewsTitle
       article={article}
       key={uuidv4()}
@@ -13,9 +28,20 @@ export const NewsContainer = (props) => {
   ));
 
   return (
-    <section>
-      { displayArticles }
-    </section>
+    <main className='news-container'>
+      <section className='left column'>
+        <h2>From the Left:</h2>
+        { displayLeft }
+      </section>
+      <section className='center column'>
+        <h2>From the Center:</h2>
+        { displayCenter }
+      </section>
+      <section className='right column'>
+        <h2>From the Right:</h2>
+        { displayRight }
+      </section>
+    </main>
   )
 };
 
--- src/reducers/articleReducer.js
+++ src/reducers/articleReducer.js
@@ -1,7 +1,7 @@
 export const articleReducer = (state=[], action) => {
   switch(action.type) {
     case 'ADD_CATEGORY_NEWS':
-    return [...action.articles] || [];
+    return action.articles || [];
     default: 
     return state
   }
