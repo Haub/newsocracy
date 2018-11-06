011f1c10a9a8294db90cd7f2413a7edbf40e842f
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-11-05 17:41:13 -0700

    Add NewsArticle component that will render individual articles

--- src/components/NewsArticle/index.js
+++ src/components/NewsArticle/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 // import { Link } from 'react-dom';
 
-const NewsArticle = ({article}) => {
-  return (
-    <div>
-      
+export const NewsArticle = ({ props }) => {
+  console.log(props)
+  return(<div><h1>'IT WORKS'</h1></div>)
+  // return (
+  //   <div>
+  //     <h1>{article.title}</h1>
+  //     <h4>{article.date}</h4>
+  //     <h4>{article.author}</h4>
+  //   </div>
 
-    </div>
-
-  )
+  // )
 }
